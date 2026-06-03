20aad085b0916024774bf5a32d106b4534b5fa00
Merdanov.Ylham <merdanowylham587@gmail.com>
2026-06-03 22:17:03 +0300

    Add files via upload

---
+++ 2.SimpleTextEditor.cpp
@@ -0,0 +1,53 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int q;
+    cin >> q;
+
+    string text = "";
+    stack<string> history;
+
+    for (int i = 0; i < q; i++)
+    {
+        int type;
+        cin >> type;
+
+        if (type == 1)
+        {
+            string w;
+            cin >> w;
+
+            history.push(text);
+            text += w;
+        }
+        else if (type == 2)
+        {
+            int k;
+            cin >> k;
+
+            history.push(text);
+            text.erase(text.size() - k);
+        }
+        else if (type == 3)
+        {
+            int k;
+            cin >> k;
+
+            cout << text[k - 1] << endl;
+        }
+        else if (type == 4)
+        {
+            text = history.top();
+            history.pop();
+        }
+    }
+
+    return 0;
+}
