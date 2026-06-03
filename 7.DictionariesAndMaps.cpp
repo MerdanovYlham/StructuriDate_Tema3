1689bec364ab81b15d206a79ee857e5a22a6e02b
Merdanov.Ylham <merdanowylham587@gmail.com>
2026-06-03 22:24:32 +0300

    Add files via upload

---
+++ 7.DictionariesAndMaps.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    unordered_map<string, string> phoneBook;
+
+    for (int i = 0; i < n; i++)
+    {
+        string name, phone;
+        cin >> name >> phone;
+
+        phoneBook[name] = phone;
+    }
+
+    string query;
+
+    while (cin >> query)
+    {
+        if (phoneBook.find(query) != phoneBook.end())
+        {
+            cout << query << "=" << phoneBook[query] << endl;
+        }
+        else
+        {
+            cout << "Not found" << endl;
+        }
+    }
+
+    return 0;
+}
